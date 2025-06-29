4a9442faf6c59acf7448c04b293c6defdc9ec25a
sharma2612 <144610168+sharma2612@users.noreply.github.com>
2025-06-29 21:15:28 +0530

    Update db.js

--- server/backend/config/db.js
+++ server/backend/config/db.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect('{ Add your mongodb URI Here }/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://sumitksharma363:sumit363@cluster4.ctpyquj.mongodb.net/food-del').then(()=>console.log("DB Connected"));
    
 }
 
-
-// add your mongoDB connection string above.
-// Do not use '@' symbol in your databse user's password else it will show an error.
